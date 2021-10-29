d5e28ae65e352438c780d67194d5c18155c086c5
Basila123 <basilanoushad92@gmail.com>
2021-10-29 15:11:23 +0530

    Added: able to insert the value

--- src/com/bridgelabzAddress/AddressList.java
+++ src/com/bridgelabzAddress/AddressList.java
@@ -1,43 +1,84 @@
 package com.bridgelabzAddress;
 import java.util.*;
-public class AddressList {
+public class AddressList  {
 
     public static void main(String[] args){
-         String firstname= " ";
-         String lastname= " ";
-         String address= " ";
-         String city= " ";
-         String state =" ";
-         String zip= " ";
-         String email= " ";
-         int phno= 0;
-         Scanner s=new Scanner(System.in);
-         Scanner s1=new Scanner(System.in);
-         System.out.println("Enter your first name");
-         firstname=s.next();
-         System.out.println("Enter your last name");
-         lastname=s.next();
-         System.out.println("Enter your address");
-         address=s.next();
-         System.out.println("Enter your city");
-         city=s.next();
-         System.out.println("Enter your state");
-         state=s.next();
-         System.out.println("Enter your zip");
-         zip=s.next();
-         System.out.println("Enter your email");
-         email=s.next();
-         System.out.println("Enter your phnone number");
-         phno=s1.nextInt();
-         System.out.println("First name:"+firstname);
-        System.out.println("Last name:"+lastname);
-        System.out.println("Address:"+address);
-        System.out.println("City:"+city);
-        System.out.println("State:"+state);
-        System.out.println("Zip"+zip);
-        System.out.println("Email:"+email);
-        System.out.println("Phone Number:"+phno);
+        Collection<ContactPerson>Co=new ArrayList<ContactPerson>();
+        Scanner s = new Scanner(System.in);
+        Scanner s1 = new Scanner(System.in);
+        System.out.println("Enter your first name");
+        String firstname = s.next();
+        System.out.println("Enter your last name");
+        String lastname = s.next();
+        System.out.println("Enter your address");
+        String address = s.next();
+        System.out.println("Enter your city");
+        String city = s.next();
+        System.out.println("Enter your state");
+        String state = s.next();
+        System.out.println("Enter your zip");
+        String zip = s.next();
+        System.out.println("Enter your email");
+        String email = s.next();
+        System.out.println("Enter your phnone number");
+        int phno = s1.nextInt();
+        System.out.println("First name:" + firstname);
+        System.out.println("Last name:" + lastname);
+        System.out.println("Address:" + address);
+        System.out.println("City:" + city);
+        System.out.println("State:" + state);
+        System.out.println("Zip" + zip);
+        System.out.println("Email:" + email);
+        System.out.println("Phone Number:" + phno);
+    }
+    }
 
 
+class ContactPerson{
+     String firstname= " ";
+     String lastname= " ";
+     String address= " ";
+     String city= " ";
+      String state =" ";
+     String zip= " ";
+    String email= " ";
+     int phno= 0;
+    ContactPerson(String fname,String lname,String address,String city, String state,String zip,String mail,int phno){
+        this.firstname=fname;
+        this.lastname=lname;
+        this.address=address;
+        this.city=city;
+        this.state=state;
+        this.zip=zip;
+        this.email=mail;
+        this.phno=phno;
+    }
+    public String getFirstname(){
+        return firstname;
+    }
+    public String getLastnamename(){
+        return lastname;
+    }
+    public String getAddress(){
+        return address;
+    }
+    public String getCity(){
+        return city;
+    }
+    public String getZip(){
+        return zip;
+    }
+    public String getState(){
+        return state;
+    }
+    public String getEmail(){
+        return  email;
+    }
+    public int getPhno() {
+        return phno;
+    }
+    //Over riding
+    public String toString(){
+        return firstname+" "+lastname+" "+ address+" "+city+" "+ zip+" "+ email+" "+phno;
     }
 }
